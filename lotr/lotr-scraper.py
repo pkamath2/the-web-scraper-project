ca07448e6b69f89b1713b0006a2e860633ecff1f
pkamath2 <git.pkamath2@gmail.com>
2017-07-04 20:27:47 +0800

    first commit

---
+++ lotr/lotr-scraper.py
@@ -0,0 +1,32 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+
+def get_lotr_characters(characters_soup, lotr_file):
+    all_divs_for_charList = characters_soup.findAll("div", class_="listchar")
+    for character in all_divs_for_charList:
+        character_name = character.text
+        character_type = ''
+        if "hobbit" in character["class"]:
+            character_type = "hobbit"
+        if "man" in character["class"]:
+            character_type = "man"
+        if "elf" in character["class"]:
+            character_type = "elf"
+        if "istari" in character["class"]:
+            character_type = "Istari"
+        if "dwarf" in character["class"]:
+            character_type = "dwarf"
+        if len(character_name) > 0:
+            lotr_file.write(character_name.strip())
+            lotr_file.write(",")
+            lotr_file.write(character_type)
+            lotr_file.write("\n")
+
+
+lotr_file = open('lotr_characters.csv','w')
+html = urlopen('http://lotrproject.com/char/List')
+html_soup = BeautifulSoup(html, 'html.parser')
+get_lotr_characters(html_soup,lotr_file)
+lotr_file.close()
---
+++ lotr/lotr_characters.py
@@ -0,0 +1,888 @@
+Adalbert Bolger,hobbit
+Adaldrida Bolger,hobbit
+Adalgar Bolger,hobbit
+Adalgrim Took,hobbit
+Adamanta Chubb,hobbit
+Adanel,man
+Adelard Took,hobbit
+Adrahil I,man
+Adrahil II,man
+Aegnor,elf
+Aerandir,elf
+Aerin,man
+Agathor,man
+Aghan,
+Aglahad,man
+Ailinel,man
+Alatar,Istari
+Aldamir,man
+Aldarion,man
+Aldor the Old,man
+Alfrida of the Yale,hobbit
+Almarian,man
+Almiel,man
+Amandil,man
+Amaranth Brandybuck,hobbit
+Amarië,elf
+Amdí­r,elf
+Amethyst Hornblower,hobbit
+Amlach,man
+Amlaith,man
+Amras,elf
+Amrod,elf
+Amroth,elf
+Amrothos,man
+Anardil,man
+Anárion,man
+Anborn,man
+Ancalagon the Black,
+Andreth,man
+Andróg,man
+Andví­r,man
+Andwise Roper,hobbit
+Angamaitë,man
+Angbor,man
+Angelica Baggins,hobbit
+Angelimir,man
+Angrim,man
+Angrod,elf
+Annael,elf
+Anson Roper,hobbit
+Ar-Adûnakhôr,man
+Ar-Gimilzôr,man
+Ar-Pharazôn,man
+Ar-Sakalthôr,man
+Ar-Zimrathôn,man
+Arachon,man
+Arador,man
+Araglas,man
+Aragorn I,man
+Aragorn Elessar,man
+Aragost,man
+Arahad I,man
+Arahad II,man
+Arahael,man
+Aranarth,man
+Arantar,man
+Aranuir,man
+Aranwë,elf
+Araphant,man
+Araphor,man
+Arassuil,man
+Aratan,man
+Arathorn I,man
+Arathorn II,man
+Araval,man
+Aravir,man
+Aravorn,man
+Arciryas,man
+Ardamir,man
+Aredhel Ar-Feiniel,elf
+Argeleb I,man
+Argeleb II,man
+Argon,elf
+Argonui,man
+Arien,
+Arminas,elf
+Artamir,man
+Arthad,man
+Arvedui,man
+Arvegil,man
+Arveleg I,man
+Arveleg II,man
+Arwen Undómiel,
+Asgon,man
+Asphodel Brandybuck,hobbit
+Atanalcar,man
+Atanatar I,man
+Atanatar II,man
+Aulë,
+Aulendil,man
+Avranc,man
+Axantur,man
+Azaghâl,dwarf
+Azog,
+Bain,man
+Balbo Baggins,hobbit
+Balcmeg,
+Baldor the Hapless,man
+Balin,dwarf
+Bandobras Took,hobbit
+Baragund,man
+Barahir,man
+Barahir,man
+Barahir,man
+Baran,man
+Baranor,man
+Baranor,man
+Bard the Bowman,man
+Bard II,man
+Basso Boffin,hobbit
+Beechbone,
+Belba Baggins,hobbit
+Beldir,man
+Beldis,man
+Belecthor I,man
+Belecthor II,man
+Beleg,man
+Beleg Cúthalion,elf
+Belegor,man
+Belegorn,man
+Belegund,man
+Belemir,man
+Belen,man
+Beleth,man
+Bell Goodchild,hobbit
+Belladonna Took,hobbit
+Belthil,
+Bëor the Old,man
+Beorn,man
+Beorn,man
+Bereg,man
+Beregar,man
+Beregond,man
+Beregond,man
+Berelach,man
+Beren,man
+Beren Erchamion,man
+Beren,man
+Berenhardt,man
+Bergil,man
+Beril,man
+Berilac Brandybuck,hobbit
+Bert,
+Berúthiel,man
+Berylla Boffin,hobbit
+Bifur,dwarf
+Bilbo Baggins,hobbit
+Bilbo Gardner,hobbit
+Bill Huggins,
+Bingo Baggins,hobbit
+Bodo Proudfoot,hobbit
+Bofur,dwarf
+Bolg of the North,
+Bombur,dwarf
+Bór,man
+Borin,dwarf
+Borlach,man
+Borlad,man
+Borlas,man
+Boromir,man
+Boromir,man
+Boromir,man
+Boron,man
+Borthand,man
+Bosco Boffin,hobbit
+Bowman Cotton,hobbit
+Brand,man
+Brandir the Lame,man
+Brandir,man
+Bregalad,
+Bregil,man
+Brego,man
+Bregolas,man
+Bregor,man
+Briffo Boffin,hobbit
+Brodda,man
+Brytta,man
+Buffo Boffin,hobbit
+Bungo Baggins,hobbit
+Calimehtar,man
+Calimehtar,man
+Calimendi,man
+Calimmacil,man
+Caliondo,man
+Calmacil,man
+Camellia Sackville,hobbit
+Caranthir the Dark,elf
+Carc,
+Carcharoth,
+Carl,hobbit
+Carl Cotton,hobbit
+Carnimírië,
+Castamir,man
+Celandine Brandybuck,hobbit
+Celeborn,elf
+Celeborn,
+Celebrían,elf
+Celebrimbor,elf
+Celebrindor,man
+Celegorm the Fair,elf
+Celepharn,man
+Cemendur,man
+Cemendur,man
+Ceorl,man
+Chica Chubb,hobbit
+Círdan the Shipwright,elf
+Cirion,man
+Ciryandil,man
+Ciryon,man
+Ciryon,man
+Cora Goodbody,hobbit
+Cotman,hobbit
+Cottar,hobbit
+Curufin the Crafty,elf
+Cwén,man
+Daeron,elf
+Dagnir,man
+Dáin I,dwarf
+Dáin Ironfoot,dwarf
+Dairuin,man
+Daisy Baggins,hobbit
+Daisy Gamgee,hobbit
+Daisy Gardner,hobbit
+Damrod,man
+Déagol,hobbit
+Denethor,elf
+Denethor I,man
+Denethor II,man
+Déor,man
+Derufin,man
+Diamond of Long Cleeve,hobbit
+Dina Diggle,hobbit
+Dinodas Brandybuck,hobbit
+Dior Eluchíl,
+Dior,man
+Dírhael,man
+Dís,dwarf
+Doderic Brandybuck,hobbit
+Dodinas Brandybuck,hobbit
+Donnamira Took,hobbit
+Dora Baggins,hobbit
+Dori,dwarf
+Dorlas,man
+Draugluin,
+Drogo Baggins,hobbit
+Dudo Baggins,hobbit
+Duilin,man
+Duilin,elf
+Duinhir,man
+Dúnhere,man
+Durin the Deathless,dwarf
+Durin VI,dwarf
+Durin VII,dwarf
+Dwalin,dwarf
+Ecthelion,elf
+Ecthelion I,man
+Ecthelion II,man
+Egalmoth,man
+Egalmoth,elf
+Eglantine Banks,hobbit
+Eilinel,man
+Elanor Gardner,hobbit
+Elatan,man
+Elboron,man
+Eldacar,man
+Eldacar,man
+Eldarion,man
+Eldarion,man
+Eldathorn,man
+Elemmakil,elf
+Elendil,man
+Elendur,man
+Elendur,man
+Elenwë,elf
+Elfhild,man
+Elfstan Fairbairn,hobbit
+Elfwine the Fair,man
+Elladan,
+Elmo,elf
+Elphir,man
+Elrohir,
+Elrond Peredhel,
+Elros Tar-Minyatur,
+Eltas,elf
+Eluréd,
+Elurín,
+Elwing,
+Emeldir the Manhearted,man
+Enel,elf
+Enelyë,elf
+Enthor,man
+Eofor,man
+Eoh,man
+Eomer,man
+Éomund of Eastfold,man
+Eorl the Young,man
+Éothain,man
+Éowyn,man
+Eradan,man
+Erchirion,man
+Erellont,elf
+Erendis,man
+Erestor,elf
+Eriol,man
+Erkenbrand,man
+Erling,hobbit
+Eru Ilúvatar,
+Esmeralda Took,hobbit
+Estë,
+Estella Bolger,hobbit
+Everard Took,hobbit
+Eärendil the Mariner,
+Eärendil,man
+Eärendur,man
+Eärendur,man
+Eärendur,man
+Eärnil,man
+Eärnil,man
+Eärnur,man
+Eärwen,elf
+Eöl,elf
+Eönwë,
+Falathar,elf
+Falco Chubb-Baggins,hobbit
+Fangluin,dwarf
+Faramir,man
+Faramir,man
+Faramir Took I,hobbit
+Farin,dwarf
+Fastolph Bolger,hobbit
+Fastred,man
+Fastred of Greenholm,hobbit
+Fëanor,elf
+Fengel,man
+Ferumbras Took II,hobbit
+Ferumbras Took III,hobbit
+Fíli,dwarf
+Filibert Bolger,hobbit
+Fimbrethil,
+Finarfin,elf
+Findegil,man
+Findis,elf
+Finduilas,elf
+Finduilas,man
+Finglas,
+Fingolfin,elf
+Fingon the Valiant,elf
+Finrod Felagund,elf
+Finwë,elf
+Fíriel,man
+Fíriel,man
+Fíriel Fairbairn,hobbit
+Fladrif,
+Flambard Took,hobbit
+Flói,dwarf
+Folca,man
+Folcred,man
+Folcwine,man
+Forlong,man
+Forodacil,man
+Forthwini,man
+Fortinbras Took I,hobbit
+Fortinbras Took II,hobbit
+Fosco Baggins,hobbit
+Fram,man
+Frár,dwarf
+Fréa,man
+Fréaláf Hildeson,man
+Fréawine,man
+Fréca,man
+Fredegar Bolger,hobbit
+Frerin,dwarf
+Frodo Baggins,hobbit
+Frodo Gardner,hobbit
+Frór,dwarf
+Frumgar,man
+Fuinur,man
+Fundin,dwarf
+Galadhon,elf
+Galador,
+Galadriel,elf
+Galathil,elf
+Galathilion,
+Galdor the Tall,man
+Galdor,elf
+Galion,elf
+Gálmód,man
+Gamil Zirak,dwarf
+Gamling,man
+Gandalf,Istari
+Gárulf,man
+Gelmir,elf
+Gelmir,elf
+Gerda Boffin,hobbit
+Gerontius Took,hobbit
+Ghân-buri-Ghân,
+Ereinion Gil-galad,elf
+Gilbarcid,man
+Gildis,man
+Gildor,man
+Gilfanon,elf
+Gilly Brownlock,hobbit
+Gilmith,
+Gilraen,man
+Gilwen,man
+Gimilkhâd,man
+Gimilzagar,man
+Gimli Elf-friend,dwarf
+Girion,man
+Glaurung,
+Gléowine,man
+Glingal,
+Glirhuin,man
+Glóin,dwarf
+Glóin,dwarf
+Glóredhel,man
+Glorfindel,elf
+Golasgil,man
+Goldberry,
+Goldilocks Gardner,hobbit
+Goldwine,man
+Golfimbul,
+Gorbadoc Brandybuck,hobbit
+Gorbag,
+Gorhendad Oldbuck,hobbit
+Gorlim,man
+Gormadoc "Deepdelver" Brandybuck,hobbit
+Gothmog,
+Gram,man
+Great Eagle,
+Great Goblin,
+Griffo Boffin,hobbit
+Grim,man
+Gríma Wormtongue,man
+Grimbeorn the Old,man
+Grimbold,man
+Grishnákh,
+Gróin,dwarf
+Grór,dwarf
+Gruffo Boffin,hobbit
+Guilin,elf
+Gundabald Bolger,hobbit
+Gundahad Bolger,hobbit
+Gundahar Bolger,hobbit
+Gundolpho Bolger,hobbit
+Guthláf,man
+Gwaihir the Windlord,
+Gwindor,elf
+Hador Lórindol,man
+Hador,man
+Halbarad,man
+Haldad,man
+Haldan,man
+Haldar,man
+Haldir,man
+Haldir,elf
+Haleth the Hunter,man
+Haleth,man
+Halfred Gamgee,hobbit
+Halfred Gamgee,hobbit
+Halfred Greenhand,hobbit
+Hallacar,man
+Hallas,man
+Hallatan,man
+Halmir,man
+Háma,man
+Háma,man
+Hamfast,hobbit
+Hamfast of Gamwich,hobbit
+Hamfast Gardner,hobbit
+Hamson Gamgee,hobbit
+Handir,man
+Hanna Goldworthy,hobbit
+Hardang,man
+Harding of the Hill,hobbit
+Hareth,man
+Hathaldir,man
+Hathol,man
+Hatholdir,man
+Heden,man
+Helm Hammerhand,man
+Henderch,man
+Hending,hobbit
+Hendor,elf
+Hengest,man
+Heorrenda,man
+Heribald Bolger,hobbit
+Herion,man
+Herugar Bolger,hobbit
+Herulcamo,man
+Herumor,man
+Hild,man
+Hilda Bracegirdle,hobbit
+Hildibrand Took,hobbit
+Hildifons Took,hobbit
+Hildigard Took,hobbit
+Hildigrim Took,hobbit
+Hirgon,man
+Hiril,man
+Hirluin,man
+Hirwen,man
+Hob Gammidge,hobbit
+Hobson,hobbit
+Holfast Gardner,hobbit
+Holman the Greenhanded,hobbit
+Holman Cotton,hobbit
+Holman Greenhand,hobbit
+Horn,man
+Horsa,man
+Huan,
+Hugo Boffin,hobbit
+Hundad,man
+Hundar,man
+Hunleth,man
+Hunthor,man
+Huor,man
+Húrin Thalion,man
+Húrin of Emyn Arnen,man
+Húrin,man
+Húrin I,man
+Húrin II,man
+Hyarmendacil I,man
+Hyarmendacil II,man
+Îbal,man
+Ibun,dwarf
+Idril Celebrindal,elf
+Ilberic Brandybuck,hobbit
+Ilmarë,
+Imin,elf
+Iminyë,elf
+Imlach,man
+Imrahil,man
+Imrazôr,man
+Indis,elf
+Indor,man
+Ingil,elf
+Ingold,man
+Ingwë Ingweron,elf
+Inzilbêth,man
+Ioreth,man
+Írildë,man
+Irimë,elf
+Irmo,
+Isembard Took,hobbit
+Isembold Took,hobbit
+Isengrim Took II,hobbit
+Isengrim Took III,hobbit
+Isildur,man
+Isilmë,man
+Isilmo,man
+Isumbras Took III,hobbit
+Isumbras Took IV,hobbit
+Ithilbor,elf
+Ivorwen,man
+Ivriniel,man
+Ivy Goodenough,hobbit
+Jessamine Boffin,hobbit
+Khím,dwarf
+Kíli,dwarf
+Lagduf,
+Lalaith,man
+Lalia Clayhanger,hobbit
+Landroval,
+Largo Baggins,hobbit
+Lassemista,
+Laura Grubb,hobbit
+Laurelin,
+Laurinquë,
+Lavender Grubb,hobbit
+Legolas,elf
+Legolas,elf
+Lenwë,elf
+Léod,man
+Lily Baggins,hobbit
+Lily Brown,hobbit
+Linda Baggins,hobbit
+Lindir,elf
+Lindissë,man
+Lindórië,man
+Lobelia Sackville-Baggins,hobbit
+Longo Baggins,hobbit
+Lóni,dwarf
+Lothíriel,man
+Lotho Sackville-Baggins,hobbit
+Lug,
+Lugdush,
+Lúthien Tinúviel,elf
+Mablung,elf
+Mablung,man
+Madoc "Proudneck" Brandybuck,hobbit
+Maedhros the Tall,elf
+Maeglin,elf
+Maglor,elf
+Magor,man
+Mahtan,elf
+Malach Aradan,man
+Malantur,man
+Mallor,man
+Malva Headstrong,hobbit
+Malvegil,man
+Mandos,
+Manthor,man
+Manwë Súlimo,
+Manwendil,man
+Marach,man
+Mardil Voronwë,man
+Marhari,man
+Marhwini,man
+Mairen,man
+Marigold Gamgee,hobbit
+Marmadoc Brandybuck,hobbit
+Marroc Brandybuck,hobbit
+Master of Lake-town,man
+Mauhúr,
+May,hobbit
+May Gamgee,hobbit
+Meleth,man
+Melian,
+Menegilda Goold,hobbit
+Meneldil,man
+Meneldor,
+Meriadoc Brandybuck,hobbit
+Merimac Brandybuck,hobbit
+Merry Gardner,hobbit
+Milo Burrows,hobbit
+Mimosa Bunce,hobbit
+Minalcar,man
+Minardil,man
+Minastan,man
+Minohtar,man
+Minto Burrows,hobbit
+Mirabella Took,hobbit
+Míriel Serindë,elf
+Mithrellas,elf
+Mí­m,dwarf
+Morgoth,
+Moro Burrows,hobbit
+Morwen Eledhwen,man
+Morwen Steelsheen,man
+Morwen,man
+Mosco Burrows,hobbit
+Mungo Baggins,hobbit
+Muzgash,
+Myrtle Burrows,hobbit
+Naimi,man
+Náin,dwarf
+Náin I,dwarf
+Náin II,dwarf
+Náli,dwarf
+Nár,dwarf
+Narmacil I,man
+Narmacil II,man
+Nerdanel the Wise,elf
+Nessa,
+Nessanië,man
+Nienna,
+Nienor Níniel,man
+Nimloth,elf
+Nimloth,
+Nimrodel,elf
+Nina Lightfoot,hobbit
+Nolondil,man
+Nora Bolger,hobbit
+Nori,dwarf
+Númendil,man
+Núneth,man
+Odo Proudfoot,hobbit
+Odovacar Bolger,hobbit
+Óin,dwarf
+Óin,dwarf
+Old Man Willow,
+Olo Proudfoot,hobbit
+Olwë,elf
+Ondoher,man
+Orchaldor,man
+Orcobal,
+Ori,dwarf
+Ornendil,man
+Orodreth,elf
+Orodreth,man
+Orodreth,man
+Orofarnë,
+Oromë,
+Oromendil,man
+Orontor,man
+Oropher,elf
+Orophin,elf
+Ossë,
+Ostoher,man
+Ostoher,man
+Others,hobbit
+Otho Sackville-Baggins,hobbit
+Othrod,
+Otto Boffin,hobbit
+Paladin Took II,hobbit
+Pallando,Istari
+Pansy Baggins,hobbit
+Pearl Took,hobbit
+Pelendur,man
+Pengolodh,elf
+Penlod,elf
+Peony Baggins,hobbit
+Peregrin Took I,hobbit
+Pervinca Took,hobbit
+Pimpernel Took,hobbit
+Pippin Gardner,hobbit
+Polo Baggins,hobbit
+Ponto Baggins I,hobbit
+Ponto Baggins II,hobbit
+Poppy Chubb-Baggins,hobbit
+Porto Baggins,hobbit
+Posco Baggins,hobbit
+Primrose Gardner,hobbit
+Primula Brandybuck,hobbit
+Prisca Baggins,hobbit
+Radagast the Brown,Istari
+Radbug,
+Radhruin,man
+Reginard Took,hobbit
+Rían,man
+Rían,man
+Robin Gardner,hobbit
+Rog,elf
+Rómendacil I,man
+Rómendacil II,man
+Rorimac Brandybuck,hobbit
+Rosa Baggins,hobbit
+Rosamunda Took,hobbit
+Rose,hobbit
+Rose Cotton,hobbit
+Rose Gardner,hobbit
+Rowan,hobbit
+Roäc,
+Ruby Bolger,hobbit
+Ruby Gardner,hobbit
+Rudibert Bolger,hobbit
+Rudigar Bolger,hobbit
+Rudolph Bolger,hobbit
+Rufus Burrows,hobbit
+Rúmil,elf
+Rúmil,elf
+Sadoc Brandybuck,hobbit
+Saeros,elf
+Salgant,elf
+Salmar,
+Salvia Brandybuck,hobbit
+Samwise Gamgee,hobbit
+Sancho Proudfoot,hobbit
+Sangahyando,man
+Sapphira Brockhouse,hobbit
+Saradas Brandybuck,hobbit
+Saradoc,hobbit
+Saruman,Istari
+Sauron,
+Scatha,
+Seredic Brandybuck,hobbit
+Shagrat,
+Shelob,
+Sigismond Took,hobbit
+Silmariën,man
+Silmo,
+Siriondil,man
+Siriondil,man
+Smaug,
+Sméagol,hobbit
+Snaga,
+Snaga,
+Soronto,man
+Tanta Hornblower,hobbit
+Tar-Alcarin,man
+Tar-Aldarion,man
+Tar-Amandil,man
+Tar-Anárion,man
+Tar-Ancalimë,man
+Tar-Ancalimon,man
+Tar-Ardamin,man
+Tar-Atanamir,man
+Tar-Calmacil,man
+Tar-Ciryatan,man
+Tar-Elendil,man
+Tar-Meneldur,man
+Tar-Minastir,man
+Tar-Míriel,man
+Tar-Palantir,man
+Tar-Súrion,man
+Tar-Telemmaitë,man
+Tar-Telperiën,man
+Tar-Vanimeldë,man
+Tarandil,man
+Tarannon Falastur,man
+Tarcil,man
+Tarciryan,man
+Tardil,man
+Targon,man
+Tarondor,man
+Tarondor,man
+Tarostar,man
+Tata,elf
+Tatië,elf
+Telchar,dwarf
+Telemnar,man
+Telimektar,
+Telperion,
+Telumehtar Umbardacil,man
+Thengel the Thrice-renowned,man
+Theobald Bolger,hobbit
+Théoden,man
+Théodred,man
+Théodwyn,man
+Thingol,elf
+Thorin I,dwarf
+Thorin II,dwarf
+Thorin Stonehelm,dwarf
+Thorondir,man
+Thorondor,
+Thorondur,man
+Thráin I,dwarf
+Thráin II,dwarf
+Thranduil,elf
+Thrór,dwarf
+Tilion,
+Tindómiel,man
+Togo Goodbody,hobbit
+Tolman Cotton,hobbit
+Tolman Cotton, Jr.,hobbit
+Tolman Gardner,hobbit
+Tom,
+Tom Bombadil,
+Tosto Boffin,hobbit
+Treebeard,
+Tulkas the Valiant,
+Tuor,man
+Turambar,man
+Turambar,man
+Turgon the Wise,elf
+Turgon,man
+Túrin Turambar,man
+Túrin I,man
+Túrin II,man
+Uffo Boffin,hobbit
+Ufthak,
+Uglúk,
+Uinen,
+Ulbandi,
+Ulbar,man
+Uldor,man
+Ulfang,man
+Ulfast,man
+Ulmo,
+Ulwarth,man
+Ungoliant,
+Urthel,man
+Urwen,
+Vairë the Weaver,
+Valacar,man
+Valandil,man
+Valandil,man
+Valandur,man
+Vána the Ever-young,
+Varda Elentári,
+Vardamir Nólimon,man
+Vardilmë,man
+Vëantur,man
+Vidugavia,man
+Vidumavi,man
+Vorondil,man
+Voronwë Aranwion,elf
+Walda,man
+Wídfara,man
+Wilcome Cotton I,hobbit
+Wilcome Cotton II,hobbit
+Wilibald Bolger,hobbit
+Wilimar Bolger,hobbit
+Wiseman Gamwich,hobbit
+The Witch-king of Angmar,man
+Wulf,man
+Yavanna Kementári,
+Yávien,man
